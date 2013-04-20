75b30c71cf3511e6c9c34ae60ff44528d98dc580
Garrett Oreilly <oreillygarrett@gmail.com>
2013-04-20 16:29:11 -0400

    The map lives! It lives!

--- map.hs
+++ map.hs
@@ -1,14 +1,18 @@
 module Map 
     ( Map 
     , Room
-    , createRooms
+    , generateMap
     ) where
 
-import System.Random
-
 data Room = Room { number :: Int, connections :: [Int] }
+instance Show Room where
+    show r = "Room number: " ++ show (number r) ++ ", " ++ "Connected rooms: " ++ show (connections r)
 type Map = [Room]
 
-createRooms :: Int -> Map
-createRooms 0 = []
-createRooms x = let rooms = Room x [] : createRooms (x - 1)
+-- Argument provided must be 1
+generateMap :: Int -> Map
+generateMap x 
+    | x == 1 = Room 1 [2, 11, 20] : generateMap 2
+    | x == 20 = [Room 20 []]
+    | otherwise = Room x ((x + 1) : c) : generateMap (x + 1) where
+        c = if x > 10 then [] else [x + 10]
