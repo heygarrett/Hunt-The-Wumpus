1842337b1990d0b73bf9a9810922702fb31b087c
Garrett Oreilly <oreillygarrett@gmail.com>
2013-05-10 17:15:21 -0400

    added descriptive prompts

--- main.hs
+++ main.hs
@@ -8,6 +8,10 @@ import Player
 
 main :: IO ()
 main = do
+    putStrLn "Welcome to Hunt the Wumpus!"
+    putStrLn "You're job is to shoot the Wumpus with an arrow!"
+    putStrLn "But be careful, if you walk into the room with the Wumpus, he'll eat you!"
+    putStrLn "If you smell the Wumpus, it means he's in an adjacent room."
     putStrLn "How many rooms would you like the map to have? It must be an even number (at least 4)!"
     line <- fmap read getLine
     let theMap = generateMap line (div line 2)
@@ -42,16 +46,18 @@ gameLoop p m w = do
         gameLoop p m w
 
 gameOver :: Bool -> IO ()
-gameOver b =
-    if b then do
-        putStrLn "There is a moaning sound in the next room before you hear something large crash to the ground. You killed the Wumpus! You win!"
-        putStrLn "Would you like to play again? (yes/no)"
-        line <- getLine
-        if line == "yes" then
-            main
-        else if line == "no" then
-            putStrLn "Thanks for playing!"
-        else
-            putStrLn "Lolwut."
-    else
-        putStrLn "You walked into room with the wumpus! He ate you. Oops."
+gameOver b = do
+    putStrLn (if b then "There is a moaning sound in the next room before you hear something large crash to the ground. You killed the Wumpus! You win!" else "You walked into room with the wumpus! He ate you. Oops.")
+    startOver
+
+startOver :: IO ()
+startOver = do
+    putStrLn "Would you like to play again? (yes/no)"
+    line <- getLine
+    if line == "yes" then
+        main
+    else if line == "no" then
+        putStrLn "Thanks for playing!"
+    else do
+        putStrLn "Sorry, that's not an aption."
+        startOver
